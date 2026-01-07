b2cde80184886b157269521bbdb99ed1edd9a58a
ryansoloman <aptechryan@gmail.com>
2026-01-07 03:42:32 -0800

    Footer modified

--- Semester-Demo Project/main.js
+++ Semester-Demo Project/main.js
@@ -1,16 +1,25 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
+hamburger.addEventListener("click", function () {
+    // hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
 });
 
-// Close menu when clicking on a link
-const navItems = document.querySelectorAll('.nav-links li a');
+const navItems = document.querySelectorAll(".nav-links li a");
 navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navLinks.classList.remove('active');
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        // navLinks.classList.remove("active");
     });
+});
+
+// Smooth scrolling for nav bar
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 400) {
+        nav.classList.add("sticky-active");
+    } else {
+        nav.classList.remove("sticky-active");
+    }
 });
